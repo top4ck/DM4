17a56ff62e88c528657802437f43e8a0ea84a504
MAXIMATOR <dedgenimatios@gmail.com>
2023-10-21 11:38:24 +0430

    https://github.com/top4ck/DM4.git

---
+++ main.py
@@ -0,0 +1,55 @@
+import random
+
+class Student:
+    def __init__(self, name=None):
+        self.name = name
+        self.alive = True
+        self.gladness = 50
+        self.progress = 0
+
+    def to_study(self):
+        print("Time to study!")
+        self.progress += 0.12
+        self.gladness -= 5
+
+    def to_chill(self):
+        print('Rest time')
+        self.gladness += 5
+        self.progress -= 0.1
+
+    def to_sleep(self):
+        print("I will sleep!")
+        self.gladness += 3
+
+    def is_alive(self):
+        if self.progress < -0.5:
+            print("Cast out....")
+            self.alive = False
+        elif self.gladness <= 0:
+            print("Depression")
+            self.alive = False
+        elif self.gladness > 5:
+            print('Passed externally...')
+
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {self.progress}")
+
+    def live(self, day):
+        day = "Day" + str(day) + "of" + self.name + "life"
+        print(f"{day:=^50}")
+        live_cube = random.randint(1,3)
+        if live_cube == 1:
+            self.to_study()
+        if live_cube == 2:
+            self.to_sleep()
+        if live_cube == 3:
+            self.to_chill()
+        self.end_of_day()
+        self.is_alive()
+
+nick = Student(name="Nick")
+for i in range(365):
+    if nick.alive == False:
+        break
+    nick.live(i)
